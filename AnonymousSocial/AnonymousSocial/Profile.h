278eaeb2ae939816a8fe4622ee17d4723c64aeea
adervise1 <adervise1@gmail.com>
2016-11-28 13:34:56 +0900

    [create] 2016.11.28. Jihun Branch 생성

---
+++ AnonymousSocial/AnonymousSocial/Profile.h
@@ -0,0 +1,13 @@
+//
+//  Profile.h
+//  AnonymousSocial
+//
+//  Created by celeste on 2016. 11. 28..
+//  Copyright © 2016년 iosSchool. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Profile : NSObject
+
+@end
---
+++ AnonymousSocial/AnonymousSocial/Profile.m
@@ -0,0 +1,13 @@
+//
+//  Profile.m
+//  AnonymousSocial
+//
+//  Created by celeste on 2016. 11. 28..
+//  Copyright © 2016년 iosSchool. All rights reserved.
+//
+
+#import "Profile.h"
+
+@implementation Profile
+
+@end
